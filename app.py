94fb12e14bb4175b3d30f14ffdfc160194b5d72a
NgocYlvn <nguyenbich.ngoc@vn.yusen-logistics.com>
2026-07-30 21:23:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -332,10 +332,7 @@ if page == "🏠 Overview":
                 "Not Booking Yet": "#9AA9B6",
             },
         )
-           fig.update_layout(
-    xaxis_title=None,
-    yaxis_title="Customers"
-)
+        
         fig.update_traces(textposition="outside")
         
         fig.update_yaxes(
